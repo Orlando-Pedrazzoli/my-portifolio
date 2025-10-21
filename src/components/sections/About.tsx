fc879057faccdcedbbd43c90ecdd1a42177e1f26
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-10-21 11:34:10 +0100

    about.tsx updated

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -64,7 +64,7 @@ export default function About() {
           },
           {
             year: '2024',
-            title: 'Data Analyst - Accenture',
+            title: 'Analyst - Accenture',
             description:
               'Data analysis for multinational client, developing automated solutions.',
           },
